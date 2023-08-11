39a40d92e11f8fd88ac401f82553da134339881a
HomeArchbishop <1984096954@qq.com>
2023-08-11 15:55:50 +0800

    fix(client): stage center position on windows browser

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,7 +8,12 @@ const stageDiv = document.querySelector<HTMLDivElement>('#stage')
 const resizeStage = (): void => {
   // 1240 * 700
   const scale = Math.min(window.innerWidth / 1240, window.innerHeight / 700)
-  if (stageDiv !== null) { stageDiv.style.transform = `scale(${scale}, ${scale})` }
+  if (stageDiv !== null) {
+    stageDiv.style.transform = `scale(${scale}, ${scale})`
+    console.log(window.innerWidth, stageDiv.getBoundingClientRect().width)
+    stageDiv.style.top = String(`${~~((window.innerHeight - stageDiv.getBoundingClientRect().height) / 2)}px`)
+    stageDiv.style.left = String(`${~~((window.innerWidth - stageDiv.getBoundingClientRect().width) / 2)}px`)
+  }
 }
 resizeStage()
 window.addEventListener('resize', resizeStage)
