75922d70d3321d3237df67f26101bc11f7461ad6
HomeArchbishop <1984096954@qq.com>
2023-08-16 23:17:07 +0800

    feat(client): add dismission handler in GameView

--- client/src/views/GameView.tsx
+++ client/src/views/GameView.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import './GameView.less'
+import type { LobbyNoticeMsg } from '../../../share/src/types/Msg'
+import { alertsm } from '../utils/swal'
 
 function GameView (): React.ReactNode {
+  React.useEffect(() => {
+    window.csController.emitter.on('lobbynotice', ({ data }) => {
+      const msg: LobbyNoticeMsg = data
+      console.log(msg)
+      if (msg.name === 'LobbyNoticeRoomDismiss') {
+        if (window.room?.roomID === msg.data.roomID) {
+          window.routeTo('HomeView')
+          alertsm('ROOM IS DISMISSED')
+        }
+      }
+    })
+    return () => {
+      window.csController.emitter.off('lobbynotice')
+    }
+  }, [])
   return (
     <div id="gameViewContainer">
       <div id="tipArea">GAME</div>
